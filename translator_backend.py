4dd0c0bd80103bd315b70fbaf9995c60d0c85033
samusindi <samusindi@gmail.com>
2025-05-15 18:12:17 -0500

    Update translator_backend.py

--- translator_backend.py
+++ translator_backend.py
@@ -38,4 +38,6 @@ def translate():
         }), 402
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
